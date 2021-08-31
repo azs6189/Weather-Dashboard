79c3f6b2f9a6a13efd14a1efa1be56fd1dad72af
Allie Soliz <solizale@gmail.com>
2021-08-31 17:16:35 -0400

    JS: added variables apiKey

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,13 @@
 
 // SEARCH FOR CITY
 var cityInput = document.querySelector('#city-input');
-var btnMain = document.querySelectorAll('btnMain');
+var btnMain = document.querySelectorAll('#btn-main');
+
+// RECENT SEARCH LIST
+var recentSearch = document.getElementById('recentSearch');
+
+// CITY ARRAY
+var cityArray = [];
+
+// API
+var apiKey = '36eb04a79cce690607777cd877a3cb61'
